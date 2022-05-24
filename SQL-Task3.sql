2bbf94d8bab49e9b75e404122bf7b37fb47aa9e5
Sibel Uysal <ssibelluysal00@gmail.com>
2022-05-24 15:12:59 +0300

    Update SQL-Task3.sql

--- SQL-Task3.sql
+++ SQL-Task3.sql
@@ -1,23 +1,23 @@
---country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralama.
+-country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralama.
 
 SELECT *
 FROM country 
 WHERE country LIKE 'A%a
 
---country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralama
+-country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralama
 
 SELECT *
 FROM country 
 WHERE country LIKE '______%n'
 
 
---film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralama.
+-film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralama.
 
 SELECT *
 FROM film
 WHERE title ILIKE '%t%t%t%t%'
 
---film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralama.
+-film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralama.
 
 SELECT *
 FROM film
