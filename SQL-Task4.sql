de1f61e18cebad5e4a53bc28beb9dc1e54a69eb9
Sibel Uysal <ssibelluysal00@gmail.com>
2022-05-25 10:52:04 +0300

    Update SQL-Task4.sql

--- SQL-Task4.sql
+++ SQL-Task4.sql
@@ -11,10 +11,20 @@ FROM film
 
 --film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'T%' AND rating='G'
 
 
 --country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 
+SELECT COUNT(*)
+FROM country
+WHERE country LIKE  '_____'
 
 
 --city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+
+SELECT COUNT(*)
+FROM city
+WHERE city ILIKE  '%r'
